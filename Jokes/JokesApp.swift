192fb919a21278737f57f42f4f9ffe8aa2e39bfc
Galstian Vagan <gvagan@me.com>
2025-12-21 15:04:33 +0700

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Ваган Галстян on 21.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
